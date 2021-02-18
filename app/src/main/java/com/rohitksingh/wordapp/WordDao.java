c00ffeba96fcf0b26a9b7c63653075871eaddf3c
Rohit <r4rohit002@gmail.com>
2021-02-17 18:20:47 -0700

    Changed return type to LiveData

--- app/src/main/java/com/rohitksingh/wordapp/WordDao.java
+++ app/src/main/java/com/rohitksingh/wordapp/WordDao.java
@@ -4,6 +4,7 @@ import com.rohitksingh.wordapp.Word;
 
 import java.util.List;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -18,7 +19,7 @@ public interface WordDao {
     @Query("DELETE from word_table")
     public void deleteAllWords();
 
-    @Query("SELECT * from word_table")
-    public List<Word> getAllWords();
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    public LiveData<List<Word>> getAllWords();
 
 }
