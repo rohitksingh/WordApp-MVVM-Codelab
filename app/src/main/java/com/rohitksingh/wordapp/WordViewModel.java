363b3b2f90b5515b8da2d37a1b5a9a53f666a12a
Rohit <r4rohit002@gmail.com>
2021-02-17 18:51:28 -0700

    Created ViewModel

---
+++ app/src/main/java/com/rohitksingh/wordapp/WordViewModel.java
@@ -0,0 +1,30 @@
+package com.rohitksingh.wordapp;
+
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository wordRepository;
+    private LiveData<List<Word>> allWords;
+    private
+
+    WordViewModel(Application application){
+        super(application);
+        wordRepository = new WordRepository(application);
+        allWords = wordRepository.getWords();
+    }
+
+    public LiveData<List<Word>> getAllWords(){
+        return allWords;
+    }
+
+    public void addWord(Word word){
+        wordRepository.addWord(word);
+    }
+
+}
